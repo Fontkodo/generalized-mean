db955132ef748dd12245ad630d253815611614bc
Fontkodo <alexgriffinsrq@gmail.com>
2016-08-01 18:19:32 -0400

    Update genralizedMean.py
    Improved special case of Geometric Mean to use greater precision.

--- genralizedMean.py
+++ genralizedMean.py
@@ -5,9 +5,14 @@ def generalizedMean(x,e):
     if x<=0 and 0 in e:
         return 0.0
     if x==0:
-        return math.pow(math.exp(sum([math.log(n) for n in e])),1.0/len(e))
+        return math.pow(product(e),1.0/len(e))
     if x==float('-inf'):
         return float(min(e))
     if x==float('inf'):
         return float(max(e))
     return math.pow(float(sum([n**x for n in e]))/len(e),1.0/x)
+def product(e):
+    p=1
+    for i in range(len(e)):
+        p*=e[i]
+    return p
