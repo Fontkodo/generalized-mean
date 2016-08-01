cf6dc767093cf19ce3ec7a6bb08bebf131dc2b98
Fontkodo <alexgriffinsrq@gmail.com>
2016-08-01 19:31:30 -0400

    Update genralizedMean.py
    Removed dependence on extraneous external libraries.

--- genralizedMean.py
+++ genralizedMean.py
@@ -1,16 +1,15 @@
-import math
 def generalizedMean(x,e):
     if len(e)==1:
         return float(e[0])
     if x<=0 and 0 in e:
         return 0.0
     if x==0:
-        return math.pow(product(e),1.0/len(e))
+        return product(e)**(1.0/len(e))
     if x==float('-inf'):
         return float(min(e))
     if x==float('inf'):
         return float(max(e))
-    return math.pow(float(sum([n**x for n in e]))/len(e),1.0/x)
+    return (float(sum([n**x for n in e]))/len(e))**(1.0/x)
 def product(e):
     p=1
     for i in range(len(e)):
