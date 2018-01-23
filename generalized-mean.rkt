111c5c822245daa55b0a4a02cd0b1511b3f20a36
John Griffin <griffinish@gmail.com>
2018-01-22 22:57:42 -0500

    Create generalized-mean.rkt

---
+++ generalized-mean.rkt
@@ -0,0 +1,64 @@
+#lang racket/base
+
+(require (only-in racket/contract contract-out -> listof)
+         (only-in racket/function curry))
+
+(provide
+ (contract-out
+  [ generalized-mean (-> real? (listof positive?) real?)]
+  [ geometric-mean   (-> (listof positive?) real? )]
+  [ harmonic-mean    (-> (listof positive?) real? )]
+  [ quadratic-mean   (-> (listof positive?) real? )]))
+
+(define (avg nums)
+  (/ (apply + nums)
+     (length nums)))
+
+(define (generalized-mean exponent nums)
+  (if (zero? exponent)
+      (expt (apply * nums)
+            (/ 1 (length nums)))
+      (expt (avg (for/list ((n (in-list nums)))
+                   (expt n exponent)))
+            (/ 1 exponent))))
+
+(define geometric-mean (curry generalized-mean  0))
+(define harmonic-mean  (curry generalized-mean -1))
+(define quadratic-mean (curry generalized-mean  2))
+
+
+
+;==============================================
+
+(module+ main
+  (define stuff '(1 10 8 3 5 6 10 9 8))
+  (for ((exponent (in-range -5 5 .5)))
+    (printf "~a\t~a\n" exponent (generalized-mean exponent stuff))))
+
+;==============================================
+
+(module+ test
+  (require rackunit)
+  
+  (define stuff '(1 10 8 3 5 6 10 9 8))
+  
+  (check-equal?
+   (avg stuff)
+   (generalized-mean 1 stuff))
+
+  (check-equal?
+   (generalized-mean 0 stuff)
+   (geometric-mean stuff))
+
+  (check-equal?
+   (generalized-mean -1 stuff)
+   (harmonic-mean stuff))
+
+  (check-equal?
+   (generalized-mean 2 stuff)
+   (quadratic-mean stuff))
+  
+  (check-true
+   (< (generalized-mean 1 stuff)
+      (generalized-mean 2 stuff)
+      (generalized-mean 3 stuff))))
